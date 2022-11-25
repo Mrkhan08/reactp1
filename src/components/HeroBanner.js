e3f59e1b458b302a6dfe96136d60136bc9580e9d
Mrkhan08 <mhk992778@gmail.com>
2022-11-25 03:58:02 -0800

    githubp1

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Box, Stack, Typography } from '@mui/material';
+
+const HeroBanner = () => {
+  return (
+    <Box>
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+        Fintness Club
+    </Typography>
+</Box>
+  
+  )
+}
+
+export default HeroBanner
